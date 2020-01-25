de54e093f8e5d82a4ddd82f7e0b62fde10bbe8d4
Kallinteris Andreas <kallinteris@protonmail.com>
2020-01-25 18:40:37 +0200

    init

---
+++ src/main/java/CVOPT_SAGS.java
@@ -0,0 +1,24 @@
+public class CVOPT_SASG {
+
+    public static void main(String[] args) throws Exception {
+
+        // the port to connect to
+        final int port;
+        try {
+            final ParameterTool params = ParameterTool.fromArgs(args);
+            port = params.getInt("port");
+        } catch (Exception e) {
+            System.err.println("No port specified. Please run 'SocketWindowWordCount --port <port>'");
+            return;
+        }
+
+        // get the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // get input data by connecting to the socket
+        DataStream<String> text = env.socketTextStream("localhost", port, "\n");
+
+        env.execute("Socket Window WordCount");
+    }
+}
+
